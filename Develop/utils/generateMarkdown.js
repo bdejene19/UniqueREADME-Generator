af4a4e42abee93b33aed9d204e26c099815fde1b
Bemnet Dejene <bdejene19@gmail.com>
2022-02-11 18:51:39 -0500

    Finished technical work of project. Finishing readme now

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license !== undefined && license !== null) {
+  if (license !== '') {
     // licenseContent = `### License Badeges \n${license}`;
     return `
       \n<img src=https://img.shields.io/badge/License-${license}-orange.svg alt=${license}>  
@@ -31,11 +31,10 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(data) {
   
-  if (data === null || data === undefined || data === '') {
+  if (data.licenses === null || data.licenses === undefined || data.licenses === '') {
     return '';
 
   } else {
-    console.log('my data: ',data);
     return `## Licenses: \n ${data.licenses}\n ${renderLicenseBadge(data.licenses)}\n${renderLicenseLink(data.licenseLinks)}`
 
   }
@@ -57,6 +56,7 @@ function generateMarkdown(data) {
   * [Description](#description)</a>\n
   * [Installation Instructions](#installation-instructions)\n
   * [Usage Info](#usage-info)\n
+  * [Contribution Guidelines](#contribution-guidelines)\n
   * [Test Instructions](#test-instructions)\n
   * [Questions](#questions)
 
